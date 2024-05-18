e456d33ea6ba2e068bbf7ee59bab1b54a1349553
LarbiWayne <chentouf.larbi.cl@gmail.com>
2024-05-18 19:54:18 +0100

    Update

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link'
 
 import { Header } from '../../../payload/payload-types'
 import { fetchHeader } from '../../_api/fetchGlobals'
-import  HeaderComponent  from './HeaderComponent'
+import HeaderComponent from './HeaderComponent'
 
 export async function Header() {
   let header: Header | null = null
@@ -20,7 +20,7 @@ export async function Header() {
 
   return (
     <>
-   <HeaderComponent header={header}/>
+      <HeaderComponent header={header} />
     </>
   )
 }
